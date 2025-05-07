c06eb9cdc9b3d8836c868c19cd2cb5578664dfc5
saketkarnik79 <saket@optimistikinfo.com>
2025-05-07 08:28:05 +0530

    Updates

--- CatalogService/obj/Debug/net8.0/CatalogService.AssemblyInfo.cs
+++ CatalogService/obj/Debug/net8.0/CatalogService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2baa619cb411f823d4a33bcf353825cfb9f4bb5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a86d2a09f618e2a1fdd6f207378a77fed7d701")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
+++ OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2baa619cb411f823d4a33bcf353825cfb9f4bb5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a86d2a09f618e2a1fdd6f207378a77fed7d701")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
